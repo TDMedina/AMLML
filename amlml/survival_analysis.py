2b17a0d6e760cea50f80046b3838c2777c17c453
TDMedina <tylerdanmedina@gmail.com>
2025-10-16 21:51:43 +0100

    Updated functions.

--- amlml/survival_analysis.py
+++ amlml/survival_analysis.py
@@ -50,7 +50,8 @@ def normalize_alpha_inputs(input_data, labels):
 
 def cross_validation_run(expression_data, categorical_data, non_categorical_data,
                          outcome_data, group_data, covariate_cardinality,
-                         original_data, l1_ratio=1, iterate_alphas=True):
+                         original_data, l1_ratio=1, iterate_alphas=True,
+                         alpha_min_ratio=0.01, n_alphas=21, alphas=None):
 
     kf = StratifiedKFold(n_splits=5, random_state=0, shuffle=True)
     results = dict()
@@ -78,7 +79,10 @@ def cross_validation_run(expression_data, categorical_data, non_categorical_data
             print(f"    Calculating lasso penalties...")
             alpha_table = test_lasso_penalties(alpha_data,
                                                (outcomes_train_dur, outcomes_train_event),
-                                               l1_ratio=l1_ratio)
+                                               l1_ratio=l1_ratio,
+                                               alpha_min_ratio=alpha_min_ratio,
+                                               n_alphas=n_alphas,
+                                               alphas=alphas)
             geneset = get_non_zero_genes(alpha_table)
         else:
             geneset = {0: (slice(None), list(alpha_data.columns))}
@@ -174,68 +178,7 @@ def cross_validation_run(expression_data, categorical_data, non_categorical_data
             results[i][alpha]["risk_splits"] = risk_splits
             results[i][alpha]["km_logrank"] = logranks
             print("Done")
-
-        # outcomes_train = (outcomes_train_dur, outcomes_train_event)
-        # outcomes_val = (outcomes_val_dur, outcomes_val_event)
-        #
-        # train_args = (expression_train, categoricals_train, non_categoricals_train)
-        # val_args = (expression_val, categoricals_val, non_categoricals_val)
-        #
-        # network = SuperModel(n_genes=expression_train.shape[-1],
-        #                      n_tech=2,
-        #                      n_expansion=4,
-        #                      n_clinical=categoricals_train.shape[-1] +
-        #                                 non_categoricals_train.shape[-1],
-        #                      covariate_cardinality=covariate_cardinality,
-        #                      embedding_dims={"race": 3, "ethnicity": 3, "interaction": 3,
-        #                                      "protocol": 3},
-        #                      shrinkage_factor=10,
-        #                      minimum_size=10,
-        #                      final_size=1)
-        # model = CoxPH(network, tt.optim.Adam)
-        #
-        # batch_size = 95
-        # lrfinder = model.lr_finder(train_args, outcomes_train, batch_size, tolerance=10)
-        # # lrfinder.plot()
-        # # plt.show()
-        # lr_optim = lrfinder.get_best_lr()
-        # print(lr_optim)
-        # model.optimizer.set_lr(lr_optim)
-        #
-        # epochs = 120
-        # callbacks = [tt.callbacks.EarlyStopping(patience=30)]
-        # verbose = True
-        #
-        # log = model.fit(train_args, outcomes_train, batch_size, epochs, callbacks, verbose,
-        #                 val_data=(val_args, outcomes_val), val_batch_size=batch_size)
-        # results[i]["pll"] = model.partial_log_likelihood(val_args, outcomes_val).mean()
-        # model.compute_baseline_hazards()
-        #
-        # surv = model.predict_surv_df(val_args)
-        #
-        # outcomes_val = np.array(outcomes_val)
-        #
-        # ev = EvalSurv(surv, outcomes_val[0], outcomes_val[1], censor_surv="km")
-        # results[i]["ctd"] = ev.concordance_td()
-        #
-        # time_grid = np.linspace(outcomes_val[0].min(), outcomes_val[0].max(),
-        #                         100)
-        # brier_scores = ev.brier_score(time_grid)
-        # results[i]["ibs"] = (simpson(y=brier_scores.values, x=brier_scores.index)
-        #                      / (brier_scores.index[-1] - brier_scores.index[0]))
-        #
-        # # ev.integrated_nbll(time_grid)
-        #
-        # predictions = [float(x[0]) for x in model.predict(val_args)]
-        # km_test_df = pd.DataFrame(zip(outcomes_val[0], outcomes_val[1], predictions),
-        #                           columns=["duration", "event", "risk"])
-        # optimal_splits = optimize_survival_splits(km_test_df, n_groups=3)
-        # risk_splits = np.cumulative_sum(optimal_splits.x)
-        # # plot_survival_curves(km_test_df)
-        # logranks = iterate_logrank_tests(km_test_df)
-        # results[i]["risk_splits"] = risk_splits
-        # results[i]["km_logrank"] = logranks
-    return results
+    return results, km_test_df
 
 
 def make_results_table(results):
@@ -254,13 +197,19 @@ def make_results_table(results):
     cols = ["fold", "alpha", "gene_count", *fields, "risk_splits", "km_logrank_AB",
             "km_logrank_BC", "km_logrank_AC"]
     table = pd.DataFrame(parsed, columns=cols)
-    table["group"] = [i for _ in range(5) for i in ascii_uppercase[:19]]
+    table["group"] = [i for _ in range(len(results.keys()))
+                      for i in ascii_uppercase[:len(list(results.values())[0].keys())]]
     table.set_index(["fold", "group", "alpha"], inplace=True)
-    return parsed, table
+    return table
 
 
 def make_mean_results_table(results_table):
     table = results_table[["gene_count", "pll", "epochs", "ctd", "ibs"]].groupby("group").mean()
+    return table
+
+
+def make_median_results_table(results_table):
+    table = results_table[["gene_count", "pll", "epochs", "ctd", "ibs"]].groupby("group").median()
     return table
 
 
@@ -273,6 +222,68 @@ cv_results = cross_validation_run(
     covariate_cardinality={"race": 7, "ethnicity": 3, "protocol": 7},
     original_data=expression_table["train"]
     )
+
+
+def plot_survival_splits(results_table):
+    splits = results_table[["gene_count"]].copy()
+    splits["split1"] = [x[0] for x in results_table.risk_splits]
+    splits["split2"] = [x[1] for x in results_table.risk_splits]
+    fig = go.Figure()
+    for fold in splits.index.unique(level="fold"):
+        data = splits.loc[idx[fold, :, :],]
+        fig.add_trace(go.Bar(x=data.reset_index()["group"],
+                             y=data.split1,
+                             offsetgroup=fold,
+                             marker_color="red",
+                             marker_line_color="red"))
+        fig.add_trace(go.Bar(x=data.reset_index()["group"],
+                             y=data.split2-data.split1,
+                             offsetgroup=fold,
+                             marker_color="blue",
+                             marker_line_color="blue"))
+        fig.add_trace(go.Bar(x=data.reset_index()["group"],
+                             y=1-data.split2,
+                             offsetgroup=fold,
+                             marker_color="green",
+                             marker_line_color="green"))
+    fig.update_layout(barmode="stack")
+
+    # splits = splits.reset_index().sort_values(["group", "gene_count"])
+    # fig2 = go.Figure()
+    # fig2.add_trace(go.Scatter(x=splits.gene_count, y=splits.split1))
+    # fig2.add_trace(go.Scatter(x=splits.gene_count, y=splits.split2))
+    logranks = ["km_logrank_AB", "km_logrank_BC", "km_logrank_AC"]
+    splits = splits.join(results_table[logranks])
+    fig3 = go.Figure()
+    for logrank in logranks:
+        fig3.add_trace(go.Scatter(x=splits.gene_count, y=splits[logrank],
+                                  mode="markers"))
+    melted = pd.melt(splits[["gene_count"]+logranks], id_vars="gene_count")
+    melted["value"] = np.log10(melted["value"])
+    fig4 = px.scatter(melted, x="gene_count", y="value", color="variable")
+    fig4.add_trace(go.Scatter(x=[0, 1600], y=[np.log10(0.05)]*2, mode="lines",
+                              marker_line_color="red"))
+    return fig, fig4
+
+
+def plot_performance_metrics(results_table, plot_mean=False, plot_median=False):
+    metrics = ["epochs", "pll", "ctd", "ibs"]
+    trimmed = results_table.copy()
+    trimmed["pll"] = [max(x, -20) for x in results_table["pll"]]
+    title = "Gene counts vs metric"
+    if plot_mean:
+        trimmed = make_mean_results_table(trimmed)
+        title += " means"
+    elif plot_median:
+        trimmed = make_median_results_table(trimmed)
+        title += " medians"
+    figs = px.scatter(pd.melt(trimmed[["gene_count"] + metrics], id_vars=["gene_count"]),
+                      x="gene_count", y="value", trendline="lowess",
+                      facet_col="variable", facet_col_wrap=2)
+    figs.update_layout(title=title)
+    figs.update_yaxes(matches=None)
+    figs.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
+    return figs
 
 
 # %% Define network and model.
@@ -294,6 +305,7 @@ test_args = (expression["test"], categoricals["test"], non_categoricals["test"])
 
 
 # %% Run the model.
+
 batch_size = 190
 lrfinder = model.lr_finder(train_args, outcomes["train"], batch_size, tolerance=10)
 # lrfinder.plot()
@@ -315,6 +327,7 @@ log = model.fit(train_args, outcomes["train"], batch_size, epochs, callbacks, ve
 
 
 # %% Evaluate the model.
+
 # model.partial_log_likelihood(validation_args, outcomes["validation"]).mean()
 outcomes["test"] = [tensor(outcomes["test"].iloc[0].to_numpy(), dtype=float32),
                     tensor(outcomes["test"].iloc[1].to_numpy(), dtype=float32)]
