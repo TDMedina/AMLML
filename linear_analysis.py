e07c4a359596c3af1d837ee8c67034ba812e8323
TDMedina <tylerdanmedina@gmail.com>
2025-12-08 19:00:04 +0000

    Added analyses workflows.

---
+++ linear_analysis.py
@@ -0,0 +1,45 @@
+
+import numpy as np
+import pandas as pd
+from sksurv.linear_model import CoxnetSurvivalAnalysis, CoxPHSurvivalAnalysis
+from sksurv.metrics import concordance_index_censored as cdex
+
+from amlml.data_loader import prepare_zscore_expression, main_loader
+from amlml.cross_normalization import zscore_normalize_genes_by_group
+
+train, test = main_loader(prepare_zscore_expression)
+
+train_data = zscore_normalize_genes_by_group(train.make_expression_table())
+test_data = zscore_normalize_genes_by_group(test.make_expression_table())
+
+
+train_outcomes = np.array(list(zip([x for x in train.outcomes[1]],
+                                   [x for x in train.outcomes[0]])),
+                          dtype="bool,f")
+test_outcomes = np.array(list(zip([x for x in test.outcomes[1]],
+                                  [x for x in test.outcomes[0]])),
+                         dtype="bool,f")
+
+model = CoxnetSurvivalAnalysis(l1_ratio=1, alpha_min_ratio=1/64,
+                               n_alphas=10, alphas=None, verbose=True)
+model.fit(train_data, train_outcomes)
+
+coef = pd.DataFrame(model.coef_, index=train_data.columns,
+                    columns=np.round(model.alphas_, 5))
+
+
+def make_score_table(data, outcomes, model, name):
+    scores = pd.DataFrame([cdex(*list(zip(*outcomes)), model.predict(data, x))
+                       for x in model.alphas_],
+                      index=np.round(model.alphas_, 5),
+                      columns=["cindex", "concordant", "discordant", "tied_risk", "tied_time"])
+    scores.index.names = ["alpha"]
+    scores["genes"] = (coef > 0).sum(axis=0)
+    scores["group"] = name
+    scores = scores.reset_index().set_index(["group", "genes", "alpha"])
+    return scores
+
+
+train_scores = make_score_table(train_data, train_outcomes, model, "train")
+test_scores = make_score_table(test_data, test_outcomes, model, "test")
+scores = pd.concat([train_scores, test_scores]).sort_index()
